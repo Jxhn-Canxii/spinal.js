71fe351e29abf2f3b0e841c3b39f72e9521bb2b7
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2023-02-16 09:13:16 +0800

    Updated 16-02-2023

--- spinal.js
+++ spinal.js
@@ -1,32 +1,7 @@
 const spine = {
 	getter1: function(url,input,n){
 		$.get(XSS.url+url,input,function(data,status){;
-			switch(n){
-				case 1:
-				spine.valve3(dash.occupant,data);
-				break;
-				case 2:
-				spine.valve3(dash.booking,data);
-				break;
-				case 3:
-				spine.valve3(dash.sales,spine.comma(data));
-				break;
-				case 4:
-				spine.valve3(dash.employee,data);
-				break;
-				case 5:
-				spine.setItem(t.target,data);
-				break;
-				case 6:
-				spine.setItem(t.c,data);
-				break;
-				case 7:
-				jt.dropper(data,input);
-				break;
-				case 8:
-				jt.imbalance(data);
-				break;
-			}
+			
 		},"json");
 	},
 	getter2: function(url,input,n,t){
@@ -34,69 +9,6 @@ const spine = {
         let clear = table.clear();
         let ajaxTime= new Date().getTime();
 		$.get(XSS.url+url,input,function(data,status){
-			 if(clear){
-			 	if(n==1){
-					if(data['credit_array']){
-						let darr = [];
-						let carr = [];
-						let je = [];
-						for (var c = data['credit_array'].length - 1; c >= 0; c--) {
-							carr += data['credit_array'][c].total;
-							je += data['credit_array'][c].je;
-						}
-						for (var d = data['debit_array'].length - 1; d >= 0; d--) {
-							darr += data['credit_array'][d].je+COMMA;
-						}
-						console.log(String(carr).split(","));
-			 		}
-			 		for (var i = data['data'].length - 1; i >= 0; i--) {
-			 			jt.append(data['data'],i,table);
-			 		}
-				}		
-			 	if(n==2){
-			 		$.each(data, function(i, item) {
-						spine.valve3(jrnl.credit,spine.comma(data['credit']));
-						spine.valve3(jrnl.debit,spine.comma(data['debit']));
-						spine.valve3(jrnl.credit+'_bot',spine.comma(data['credit']));
-						spine.valve3(jrnl.debit+'_bot',spine.comma(data['debit']));
-					});
-			 	}
-			 	//for multiple array
-			 	if(n==3){
-					if(data['data']){
-						console.log(data.print);
-						if(data.print==='true'){
-							window.open(XSS.url+jour.link7,'_blank');				
-						}
-						spine.valve3(jrnl.credit,spine.comma(data['credit']));
-						spine.valve3(jrnl.debit,spine.comma(data['debit']));
-						spine.valve3(jrnl.credit+'_bot',spine.comma(data['credit']));
-						spine.valve3(jrnl.debit+'_bot',spine.comma(data['debit']));
-						for (var i = data['data'].length - 1; i >= 0; i--) {
-							jt.append(data['data'],i,table);
-						}
-					}else{
-						let message_content = (input.print) ? "Cannot print empty results" : "No results found!";
-						let message = {
-							message: message_content,
-							error: true,
-						}
-						spine.valve3(jrnl.credit,0.00);
-						spine.valve3(jrnl.debit,0.00);
-						spine.valve3(jrnl.credit+'_bot',0.00);
-						spine.valve3(jrnl.debit+'_bot',0.00);
-						spine.error_message(message,false,false);
-					}
-			 	}
-			 	if(n==4){
-			 		sheet.append(data);
-			 		$(bal.bs).LoadingOverlay("hide",true);
-			 		$(common.submit).LoadingOverlay("hide",true);
-			 		spine.valve5(common.submit,'disabled',false);
-
-			 	}
-			 }
-			 		spine.loadtime();
 			 table.draw();
 		},"json");
 	},
@@ -122,67 +34,17 @@ const spine = {
     },	
 	handler: function(id,event,n,data){
 		$(id).on(event, function() {
-			switch(n){
-				case 1:
-				return false;
-				break;
-				case 2:
-				let message_content = "Data restored";
-				let message = {
-					message: message_content,
-					error: false,
-				}
-				if(data){
-					spine.prop(jrnl.dcheck,j.check,true);
-					spine.prop(jrnl.date,j.disable,true)
-					spine.valve4(jrnl.type,j.all);
-					spine.valve4(jrnl.name,j.all);
-					spine.valve4(jrnl.acnt,j.all);
-					spine.valve4(jrnl.jeno,j.all);
-					spine.valve4(jrnl.desc,j.all);
-				}
-				spine.error_message(message,false,false);
-				journal();
-				total();
-				break;
-				case 3:
-				filter(data);
-				console.log(data);
-				break;
-				case 4:
-				let title = 'Attention!';
-				let checker =  $(common.dark).is(":checked");
-				let checked = (checker) ? DARK_MODE : '';
-				let mode = (checker===DARK_MODE || checker!='') ? "Dark mode " : "Light mode ";
-				spine.toaster(title,mode+" activated!",false,tP.bL)
-				spine.setItem(common.darker,checked);
-				break;
-			}
+			
 		});
 	},
 	timelord: function(n,time,value){
 		setInterval(function(){
-			switch(n){
-				case 1:
-				let dark = spine.getItem(common.darker);
-				(dark===DARK_MODE || dark!='') ? spine.valve3(common.blacklabel,'Dark Mode') : spine.valve3(common.blacklabel,'Light Mode');
-				(dark===DARK_MODE || dark!='') ? spine.prop(common.dark,j.check,true) : spine.prop(common.dark,j.check,false);
-				spine.valve5(common.body,DARK_MODE,dark);
-				break;
-			}
+			
 		},time);
 	},
 	timestopper: function(n,time,value){
 		setTimeout(function(){
-			switch(n){
-				case 1:
-				target();
-				chart();
-				break;
-				case 2:
-				return $.LoadingOverlay("hide");
-				break;
-			}
+			
 		},time);
 	},
 	toaster: function(title,description,res,position)
